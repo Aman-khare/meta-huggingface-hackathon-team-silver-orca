e9fdb4af85d59fc065ee949353dd989db9632764
Aman Khare <161465425+Aman-khare@users.noreply.github.com>
2026-04-08 16:19:41 +0530

    final edits for submission

--- server/app.py
+++ server/app.py
@@ -51,3 +51,10 @@ app.include_router(router)
 async def root():
     """Redirect to the FastAPI interactive documentation."""
     return RedirectResponse(url="/docs")
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
